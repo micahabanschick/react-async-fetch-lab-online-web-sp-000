be36235db749ef00056e48b8e97eb1d39873008e
Micah Banschick <micha.ban@gmail.com>
2021-03-23 17:06:23 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 // create your App component here
-import React from 'react'
+import React, { Component } from 'react';
 
-class App extends React.Component {
-  state = { peopleInSpace: [] }
+export default class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            data: null
+        }
+    }
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(res => res.json() )
+        .then(json => this.setState({data: json}))
+    }
+    render(){
 
-  render() {
-    return (
-      <div>
-        {this.state.peopleInSpace.map((person, id) => (
-          <h1 key={id} >{person.name}</h1>
-        ))}
-      </div>
-    )
-  }
-
-  componentDidMount() {
-    fetch('http://api.open-notify.org/astros.json')
-      .then(res => res.json())
-      .then(people => this.setState({ peopleInSpace: people }))
-  }
-}
-
-export default App
+    }
+} 
